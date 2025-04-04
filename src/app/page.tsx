92d87358a27192244c6593f22a35c427e309a71b
michael-bautista <mrbautistadev@gmail.com>
2025-04-03 18:50:16 -0700

    fixed marathon time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ const Home = () => {
                         Currently building an iOS app to help people run and lift.
                     </p>
                     <p className="text-sm">
-                        Obsessed with health and fitness. Division 1 soccer, SF marathon in 3:29, and 1st 
+                        Obsessed with health and fitness. Division 1 soccer, SF marathon in 3:23, and 1st 
                         place finisher in 32 mile ultra with 8,691 feet of elevation. Also into 
                         videography and content creation.
                     </p>
